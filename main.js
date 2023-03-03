978085d671d30730fc499a654890c5f17febb8f0
Олег Ткаченко <oleg-tkachenko-02@mail.ru>
2023-03-03 21:11:16 +0300

    eat shit (proffesional)

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
 
-let win;
 let win;
 
 function createWindow () {
@@ -17,44 +14,6 @@ function createWindow () {
         }
       })
 
-    mainWindow.webContents.openDevTools();  
-
-      ipcMain.on('set-minimize', (event) => {
-            const webContents = event.sender
-            const win = BrowserWindow.fromWebContents(webContents)
-            win.minimize();
-          })
-
-        ipcMain.on('set-close', (event) => {
-            const webContents = event.sender
-            const win = BrowserWindow.fromWebContents(webContents)
-            win.close();
-        })
-    
-      mainWindow.loadFile('index.html');
-    }
-    
-    app.whenReady().then(() => {
-      createWindow()
-      app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-      })
-    })
-    
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
-    })
-function createWindow () {
-      const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        frame: false,
-        resizable: false,
-        webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
-        }
-    
-      })
     mainWindow.webContents.openDevTools();  
 
       ipcMain.on('set-minimize', (event) => {
